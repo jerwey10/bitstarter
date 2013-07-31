9218f22ccb686e9cb239864956dabec2e981d785
Jerry Wolverton <jerwey10@gmail.com>
2013-07-31 08:25:34 +0000

    Connected web.js with index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var buffer = fs.readFileSync(index.html);
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
