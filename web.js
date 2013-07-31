590cc63a0f3165a4d1fc338cba4127ca06853bd8
Jerry Wolverton <jerwey10@gmail.com>
2013-07-31 08:46:31 +0000

    Connected web.js with index.html again 2

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
+
 var app = express();
 var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', 'utf-8');
+  response.send(fs.readFileSync('index.html', 'utf-8'));
 });
 
 var port = process.env.PORT || 5000;
